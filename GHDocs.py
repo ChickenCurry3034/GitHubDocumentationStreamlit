a24c3aee689bbdd13d16f732eb13a740a6a6f5eb
ChickenCurry3034 <106786326+ChickenCurry3034@users.noreply.github.com>
2022-08-31 10:52:46 -0400

    Version 2.0

--- GHDocs.py
+++ GHDocs.py
@@ -3,13 +3,14 @@ st.set_page_config(layout="wide")
 from PIL import Image
 
 st.title('GitHub Docs by Aarush')
-choice = st.selectbox("Pick the following Git Commands!",["Initialize","Status","Log","Stage","Add Origin","Commit","Pull Code","Remove Origin","Reset"])
+choice = st.selectbox("Pick the following Git Commands!",["Initialize","Status","Log","Stage","Add Origin","Commit","Push Code","Pull Code","Remove Origin","Reset"])
 dict = { "Initialize" : ["git init","This should be the first command after accessing the folder through git bash. With this command, git will be initialized in the folder."],
          "Status" : ["git status","With this command, the user can access what files are ready for the committing stage and what files still need to be staged."],
          "Log" : ["git log","With this command, the commit history of the files in the folder are shown."],
          "Stage" : ["git add .","This command puts the file into the staging area after the working phase as the next stage is to commit the file."],
          "Add Origin" : ["git remote add origin <link of repository>","This should add the origin link of the git repository, as GitHub should already have access to your account before doing this stage."],
          "Commit" : ["git commit -m 'commit name'","This command puts the file into the git repository for people to access."],
+         "Push Code" : ["git push origin main","This command allows a user to push their code into their git repository."],
          "Pull Code" : ["git pull origin main","This command allows a user to pull some code from a repository with the permission of the original owner of the GitHub repository."],
          "Remove Origin" : ["git remote remove origin","This command allows the user to remove the existing origin."],
          "Reset" : ["git reset <file>","This command allows the user to reset a certain file to its previous version."]
@@ -18,7 +19,7 @@ st.subheader("Code of the command selected:")
 st.code(dict[choice][0])
 st.caption(dict[choice][1])
 
-original_title = '<p style="font-family:Serif; color:LightBlue; font-size: 30px;">Example</p>'
+original_title = '<p style="font-family:Serif; color:LightBlue; font-size: 30px;">Visual</p>'
 st.markdown(original_title, unsafe_allow_html=True)
 
 def add_bg_from_url():
@@ -37,3 +38,20 @@ def add_bg_from_url():
 
 add_bg_from_url()
 
+if choice=="Initialize":
+    image="1.png"
+elif choice =="Status":
+    image="2.png"
+elif choice =="Stage":
+    image="4.png"
+elif choice =="Log":
+    image="3.png"
+elif choice=="Add Origin":
+    image="5.png"
+elif choice=="Commit":
+    image="6.png"
+elif choice=="Push Code":
+    image="7.png"
+elif choice=="Remove Origin":
+    image="8.png"
+st.image(image)
